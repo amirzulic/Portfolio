52f50e240f76e6cf9fb1af8dcad4c560ae3d8e43
amirzulic <56558009+amirzulic@users.noreply.github.com>
2022-10-16 14:59:44 +0200

    Implementation of contact me form (#3)

--- frontend/src/components/ContactMe.js
+++ frontend/src/components/ContactMe.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, {useRef} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../style/contact_me.css";
+import emailjs from '@emailjs/browser';
 
 function ContactMe() {
+    const form = useRef();
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('service_eg2wfdo', 'template_5yjpjjb', form.current, 'APfvGvW3gFWJIaT4D')
+            .then((result) => {
+                alert("Message sent successfully!");
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset();
+    }
+
     return(
         <div className="container" id="ContactMe">
             <div className="row">
@@ -26,19 +41,26 @@ function ContactMe() {
             <br/>
             <div className="row justify-content-center align-items-center">
                 <div className="col">
-                    <form>
+                    <form onSubmit={sendEmail} ref={form}>
                         <div className="form-group">
-                            <input type="text" className="form-control form-control-lg FormInput" id="name"
+                            <input type="text" className="form-control form-control-lg FormInput"
+                                   id="name"
+                                   name = "name"
                                    placeholder="What is your name?"/>
                         </div>
                         <br/>
                         <div className="form-group">
-                            <input type="email" className="form-control form-control-lg FormInput" id="email"
+                            <input type="email" className="form-control form-control-lg FormInput"
+                                   id="email"
+                                   name="email"
                                    placeholder="Enter your email"/>
                         </div>
                         <br/>
                         <div className="form-group">
-                            <textarea className="form-control form-control-lg FormInput" id="message" rows="6"
+                            <textarea className="form-control form-control-lg FormInput"
+                                      id="message"
+                                      name="message"
+                                      rows="6"
                             placeholder="Your message"/>
                         </div>
                         <br/>

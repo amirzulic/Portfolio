38c2b6e634bed37e7027b456b5276bf85c8d3dbf
amirzulic <nokia5130>
2022-10-18 18:53:42 +0200

    Initial changes to the Footer component

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -15,33 +15,33 @@ function Footer() {
                 <hr className="border-primary"/>
             </div>
             <div className="row">
-                <div className="col-6">
-                    <h1 className="titleFooter">Need my social media?</h1>
-                    <h2>
-                        <b className="text-primary email">
-                            zulicic0@gmail.com
-                        </b>
-                    </h2>
+                <div className="col">
+                    <div className="row">
+                        <h1 className="titleFooter">Need my social media?</h1>
+                    </div>
+                    <div className="row">
+                        <h2>
+                            <b className="text-primary email">
+                                zulicic0@gmail.com
+                            </b>
+                        </h2>
+                    </div>
                 </div>
-                <div className="col text-end">
+                <div className="col flex-row d-flex">
                     <img src={Instagram} height="20px"/>
-                    <h3 className="socialMedia">
+                    <h3 className="socialMedia px-2">
                         <a href="https://www.instagram.com/zulagaba" className="text-decoration-none" target="_blank">
                             Instagram
                         </a>
                     </h3>
-                </div>
-                <div className="col text-end">
                     <img src={LinkedIn} height="20px"/>
-                    <h3 className="socialMedia">
+                    <h3 className="socialMedia px-2">
                         <a href="https://www.linkedin.com/in/amir-zulic-80b70b199/" className="text-decoration-none" target="_blank">
                             LinkedIn
                         </a>
                     </h3>
-                </div>
-                <div className="col text-end">
                     <img src={Facebook} height="20px"/>
-                    <h3 className="socialMedia">
+                    <h3 className="socialMedia px-2">
                         <a href="https://www.facebook.com/amir.zulic9" className="text-decoration-none" target="_blank">
                             Facebook
                         </a>

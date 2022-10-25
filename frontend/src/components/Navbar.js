a9f4a7bcd83dbdf8edd5ecedf30b0a2327bc6c20
amirzulic <zulicic0@gmail.com>
2022-10-25 16:05:57 +0200

    Implementation of the navbar toggle for smaller screens

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,22 +1,22 @@
 import React from "react";
+import "bootstrap/dist/js/bootstrap.min.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "bootstrap/dist/js/bootstrap.bundle.min";
 import {Link} from "react-scroll";
 
 function Navbar() {
     return(
         <div className="container">
-            <nav className="navbar navbar-expand-lg navbar-dark fixed-top container bg-transparent">
+            <nav className="navbar navbar-expand-lg navbar-dark container bg-transparent">
                 <Link to="LandingPage">
                     <img src="https://cdn-icons-png.flaticon.com/512/5968/5968654.png"
                          width="30" height="30" alt=""/>
                 </Link>
-                {/*<button className="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
-                </button>*/}
-                <div className="collapse navbar-collapse justify-content-end">
+                </button>
+                <div className="collapse navbar-collapse justify-content-lg-end text-center" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
                             <Link className="nav-link" to="LandingPage" spy={true} smooth={true} offset={0} duration={300}>Home</Link>
